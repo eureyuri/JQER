96c8373c7d736418e3bec3189eaef1c189fbe17c
ppanwin10 <percyjxp@gmail.com>
2021-03-14 23:10:08 -0400

    replicate the mini python

---
+++ test.py
@@ -0,0 +1,22 @@
+string def make(string n):
+    if n == 0: 
+        return ""
+    else:
+        return "a" + make(n-1)
+
+
+tree()
+
+print(make(3))
+
+# print(True > False)
+# print(1 < 2)
+# print(1 == 1)
+# print([] < [1])
+# print([1] < [1,1,0])
+# print([2] > [1,1,0])
+# print("a" == "a")
+# print("bc" < "z")
+# print("bc" < "bca")
+# print("bc" > "azzz")
+# print([] != [])
