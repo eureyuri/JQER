a5b9370da04164f8c6255f016aed752cf6a4a474
eureyuri <eureynoguchi@gmail.com>
2021-03-17 22:21:58 -0400

    feat: connecting parser, scanner, and ast with addition of type declaration

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
-def make(n):
-    if n == 0: 
+# First goal
+# print("Hello")
+
+# Second goal
+string def make(int n):
+    if n == 0:
         return ""
     else:
         return "a" + make(n-1)
 
 print(make(5))
 
-print(True > False)
-print(1 < 2)
-print(1 == 1)
-print([] < [1])
-print([1] < [1,1,0])
-print([2] > [1,1,0])
-print("a" == "a")
-print("bc" < "z")
-print("bc" < "bca")
-print("bc" > "azzz")
-print([] != [])
+# print(True > False)
+# print(1 < 2)
+# print(1 == 1)
+# print([] < [1])
+# print([1] < [1,1,0])
+# print([2] > [1,1,0])
+# print("a" == "a")
+# print("bc" < "z")
+# print("bc" < "bca")
+# print("bc" > "azzz")
+# print([] != [])
